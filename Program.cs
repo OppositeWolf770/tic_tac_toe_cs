5609b5309a993007eb20ed416ce8f1105376f2ff
Daniel Hoelzeman <dhoelzeman@outlook.com>
2023-11-01 09:31:44 -0500

    Added more content.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Type alias for the 2D List
+using Board = System.Collections.Generic.List<System.Collections.Generic.List<char>>;
+
+const char  INITIAL_VALUE = 'X';
+
+Board board = [
+    [INITIAL_VALUE, INITIAL_VALUE, INITIAL_VALUE],
+    [INITIAL_VALUE, INITIAL_VALUE, INITIAL_VALUE],
+    [INITIAL_VALUE, INITIAL_VALUE, INITIAL_VALUE],
+];
+
+DisplayBoard(board);
+
+
+// Displays the board
+void DisplayBoard(Board board) {
+    foreach (var row in board) {
+        foreach (var col in row) {
+            Console.Write(col);
+        }
+        Console.WriteLine();
+    }
+}
